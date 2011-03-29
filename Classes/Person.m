ba368df7d3186b2c10f2d09bbbe82ab2ffc9d87c
Chi Ieong Kam <clka325@chil09.orchestra.cse.unsw.edu.au>
2011-03-29 15:46:16 +1100

    Commit Message...

---
+++ Classes/Person.m
@@ -0,0 +1,14 @@
+//
+//  Person.m
+//  Class1
+//
+//  Created by Chi Ieong Kam on 29/03/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "Person.h"
+
+
+@implementation Person
+
+@end
